3d3b9f28715804dd2a9b21341212dc46b43426f6
zijievv <zijie_vv@icloud.com>
2020-08-22 19:12:18 +0800

    ArcProgressView -- init

---
+++ ArcProgressView/ArcProgressView/ArcProgress.swift
@@ -0,0 +1,23 @@
+//
+//  ArcProgress.swift
+//  ArcProgressView
+//
+//  Created by zijie vv on 22/08/2020.
+//  Copyright © 2020 zijie vv. All rights reserved.
+//
+//  ============================================================================
+//
+
+import SwiftUI
+
+struct ArcProgress: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct ArcProgress_Previews: PreviewProvider {
+  static var previews: some View {
+    ArcProgress()
+  }
+}
