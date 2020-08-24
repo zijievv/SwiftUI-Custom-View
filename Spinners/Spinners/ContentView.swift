74d92a619ab7c52a7ad5f56485bd1b0808ce96a3
zijievv <zijie_vv@icloud.com>
2020-08-24 20:44:17 +0800

    Spinners -- init

---
+++ Spinners/Spinners/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Spinners
+//
+//  Created by zijie vv on 24/08/2020.
+//  Copyright © 2020 zijie vv. All rights reserved.
+//
+//  ============================================================================
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
