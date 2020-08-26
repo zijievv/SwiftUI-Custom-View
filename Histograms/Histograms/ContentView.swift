fda00570510c010f5a80e5aa5b2f430c64baa5c6
zijievv <zijie_vv@icloud.com>
2020-08-27 01:11:15 +0800

    Histograms -- init

---
+++ Histograms/Histograms/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Histograms
+//
+//  Created by zijie vv on 27/08/2020.
+//  Copyright © 2020 zijie vv. All rights reserved.
+//
+//  ============================================================================
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
